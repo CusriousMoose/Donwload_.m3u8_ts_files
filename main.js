4ac466c494ebed468399b4c51a575dc9d0043fc2
CusriousMoose <163154099+CusriousMoose@users.noreply.github.com>
2024-04-25 13:06:58 +0530

    Update main.js
    changes

--- main.js
+++ main.js
@@ -85,7 +85,7 @@ function generateSequenceURLs(api1, api2) {
     const urls = [];
     var count=0;
     for (let j = minNumber; j <= maxNumber; j++) {
-        urls.push(`${commonPrefix}${j}.ts`);
+        urls.push(`${commonPrefix}${j}.tsb`);
         count++;
     }
     document.getElementById("baseURL").value = commonPrefix;
